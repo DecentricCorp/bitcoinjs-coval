0440cd612aeb74ac62bcc7ca28557fca04b7ca84
Shannon Code <genecyber@gmail.com>
2017-01-05 09:50:15 -0500

    coval

---
+++ coval.js
@@ -0,0 +1,25 @@
+var Testnets = {};
+
+// extend known network params with dogecoin and litecoin testnets
+Testnets.coval = {
+  messagePrefix: '\x18Bitcoin Signed Message:\n',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x04358394
+  },
+  pubKeyHash: 0x3d,
+  scriptHash: 0x80,
+  wif: 241,
+  dustThreshold: 100000000,
+};
+
+var register = function (btcjslibNetObj) {
+  Object.keys(Testnets).forEach(function (net) {
+    btcjslibNetObj[net] = Testnets[net];
+  });
+};
+
+module.exports = {
+  register: register
+};
+
